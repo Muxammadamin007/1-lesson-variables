a6206d39541b4c2f9a652aa5f2a959eb6d408a7a
Muxammadamin007 <muxammadaminxakimov@gmail.com>
2024-08-19 14:16:38 +0500

    1step

---
+++ index.js
@@ -0,0 +1,35 @@
+// let a=8;
+
+// const p=10;
+
+// var b=15;
+
+// console.log(a+b+a);
+
+// // alert('18+ lar uchun')
+
+
+
+// let FoydalanuvchiniJavobi = confirm("Aniq shu mahsulotni harid qilmoqchimisz?");
+// console.log(FoydalanuvchiniJavobi);
+
+
+// alert('Aniq hamyonizda pul bormi?');
+
+// prompt('Yonizdagi pulni kririting')
+
+// let m='MeningYoshim18da';
+
+// console.log(m);
+
+
+// const p=2024
+
+// let UsersAge =  p-prompt('Yoshizni kiriting');
+
+// console.log(UsersAge);
+
+
+let UserAge = prompt('Tugilgan Yilizni Kiriting')
+
+alert('Sizning yoshingiz ' + (2024-UserAge ) + 'da ekan' )
